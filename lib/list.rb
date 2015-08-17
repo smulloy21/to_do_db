521d4f8f208917340756f9a2052bcb4a2c97a5c5
Shannon Mulloy & Tim Kellogg <student@epicodus.com>
2015-08-17 10:34:02 -0700

    add method to test equivalency

--- lib/list.rb
+++ lib/list.rb
@@ -6,5 +6,27 @@ class List
     @name = attributes.fetch(:name)
     @id   = attributes.fetch(:id)
   end
+  define_singleton_method(:all) do
+    returned_lists = DB.exec("SELECT * FROM lists;")
+    lists = []
+    returned_lists.each() do |list|
+      name = list.fetch('name')
+      id = list.fetch('id').to_i()
+      lists.push(List.new({:name => name, :id => id}))
+    end
+    lists
+  end
+  define_method(:save) do
+    result = DB.exec("INSERT INTO lists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch('id').to_i()
+  end
 
+  define_method(:==) do |another_list|
+    if self.id   == another_list.id &&
+       self.name == another_list.name
+       true
+    else
+       false 
+    end
+  end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -8,4 +8,30 @@ describe(List) do
       expect(test_list.name()).to(eq("My List"))
     end
   end
+  describe('.all') do
+    it('should be empty') do
+      expect(List.all()).to(eq([]))
+    end
+  end
+  describe('#id') do
+    it('should return the id of a list') do
+      test_list = List.new({:name => "My List", :id => nil})
+      test_list.save()
+      expect(test_list.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+  describe('#save') do
+    it('should save a list to the database') do
+      test_list = List.new({:name => "My List", :id => nil})
+      test_list.save()
+      expect(List.all()).to(eq([test_list]))
+    end
+  end
+  describe('#==') do
+    it('should return true if the ids and names are the same') do
+      test_list  = List.new({:name => "My List", :id => nil})
+      test_list2 = List.new({:name => "My List", :id => nil})
+      expect(test_list).to(eq(test_list2))
+    end
+  end
 end
