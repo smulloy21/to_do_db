d9ed27987f06e4c76e39ee8249e81a7b45e5f643
Shannon Mulloy & Tim Kellogg <student@epicodus.com>
2015-08-17 16:33:20 -0700

    wire up posting/getting routes for adding new lists and tasks

--- app.rb
+++ app.rb
@@ -1,5 +1,35 @@
-require 'sinatra' 
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/task')
+require('./lib/list')
+also_reload('lib/**/*.rb')
+require("pg")
+
+DB = PG.connect({:dbname => "to_do"})
 
 get '/'  do
+	@lists = List.all()
 	erb(:index)
+end
+
+post('/lists') do
+	name = params.fetch('list')
+	list = List.new({:name => name, :id => nil})
+	list.save()
+	redirect('/')
+end
+
+post('/task/new') do
+	id = params.fetch('id').to_i()
+	description = params.fetch('description')
+	due_date = params.fetch('due_date')
+	@list = List.find(id)
+	task = Task.new(:list_id => id, :description => description, :due_date => due_date)
+	task.save()
+	redirect('/list/' + @list.id().to_s)
+end
+
+get('/list/:id') do
+	@list = List.find(params.fetch('id'))
+	erb(:list)
 end
