9455cba5b73adbb53293ab7968c33cb5b9cddc68
Shannon Mulloy & Tim Kellogg <student@epicodus.com>
2015-08-17 16:32:10 -0700

    add the ability to find a list by its id

--- lib/list.rb
+++ lib/list.rb
@@ -31,4 +31,29 @@ class List
        false
     end
   end
+
+  define_singleton_method(:find) do |id_number|
+    id_number.to_i()
+    found_item = nil
+    returned_list = DB.exec("SELECT * FROM lists WHERE id = '#{id_number}'")
+    returned_list.each() do |list|
+      name = list.fetch('name')
+      id = list.fetch('id')
+      found_item = List.new({:name => name, :id => id})
+    end
+    found_item
+  end
+
+  define_method(:tasks) do
+    @id = self.id
+    returned_tasks = DB.exec("SELECT * FROM tasks WHERE list_id = #{@id};")
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch('description')
+      due_date = task.fetch('due_date')
+      list_id = task.fetch('list_id')
+      tasks.push(Task.new(:description => description, :due_date => due_date, :list_id => list_id))
+    end
+    tasks
+  end
 end
