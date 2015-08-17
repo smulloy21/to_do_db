3e0a9af3f76da2f69ade514d38610cc6254243e9
Shannon Mulloy & Tim Kellogg <student@epicodus.com>
2015-08-17 10:06:43 -0700

    add the ability to save the method to the database and determine if the objects are the same

--- lib/task.rb
+++ lib/task.rb
@@ -6,4 +6,30 @@ class Task
     @description = attributes.fetch(:description)
     @id          = attributes.fetch(:id)
   end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}') RETURNING id;")
+    @id    = result.first().fetch('id').to_i()
+  end
+
+  define_singleton_method(:all) do
+    returned_tasks = DB.exec("SELECT * FROM tasks;")
+    tasks = []
+    returned_tasks.each() do |task|
+      description = task.fetch('description')
+      id = task.fetch('id').to_i()
+      tasks.push(Task.new({:description => description, :id => id}))
+    end
+    tasks
+  end
+
+
+  define_method(:==) do |another_task|
+    if self.description == another_task.description &&
+       self.id          == another_task.id
+      true
+    else
+      false
+    end
+  end
 end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -9,4 +9,28 @@ describe(Task) do
       expect(test_task.description()).to(eq("A Task to Complete"))
     end
   end
+
+  describe('#id') do
+    it('should return the id of the task') do
+      test_task = Task.new({:description => "A Task to Complete", :id => nil})
+      test_task.save
+      expect(test_task.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#save') do
+    it('should store the task in the database') do
+      test_task = Task.new({:description => "A Task to Complete", :id => nil})
+      test_task.save
+      expect(Task.all()).to(eq([test_task]))
+    end
+  end
+
+  describe(':==') do
+    it('returns true if the descriptions and the ids are the same') do
+      test_task = Task.new({:description => "A Task to Complete", :id => 1})
+      test_task2 = Task.new({:description => "A Task to Complete", :id => 1})
+      expect(test_task).to(eq(test_task2))
+    end
+  end
 end
